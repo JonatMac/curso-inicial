9f1bb8d87a02682a53cd0b2a8eff489641ea5c28
JonatMac <jonathan15-08@hotmail.com>
2023-04-19 17:57:49 -0500

    Cambios 20230419

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoadingComponent } from './components/loading/loading.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { ButtonModule } from 'primeng/button';
 import { PaginaTablaComponent } from './pages/pagina-tabla/pagina-tabla.component';
+import { TableModule } from 'primeng/table';
+import { PanelModule } from 'primeng/panel';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuHeaderComponent } from './components/menu-header/menu-header.component';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     LoadingComponent,
-    PaginaTablaComponent
+    PaginaTablaComponent,
+    MenuHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ButtonModule
+    ButtonModule,
+    HttpClientModule,
+    TableModule,
+    PanelModule,
+    TabMenuModule,
+    MenubarModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/pagina-tabla/pagina-tabla.component.ts
+++ src/app/pages/pagina-tabla/pagina-tabla.component.ts
@@ -1,17 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IDataEmpleado } from 'src/app/interfaces/empleadosInterface';
+import { EmpleadoService } from 'src/app/services/empleado.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-pagina-tabla',
   templateUrl: './pagina-tabla.component.html',
   styleUrls: ['./pagina-tabla.component.css']
 })
-export class PaginaTablaComponent {
-  constructor(private rutas: Router){
-
+export class PaginaTablaComponent implements OnInit {
+  listEmpleado: IDataEmpleado[] = [];
+  columnTabla: any;
+  
+  constructor(private rutas: Router,
+              private empleadoService: EmpleadoService,
+              private mensajes: MessageService){
   }
-RegresarInicio(){
-  //llamada de la ruta para el inicio
-  this.rutas.navigate(['inicio']);
-}
+
+  ngOnInit(): void {
+    this.iniColumnaTabla();
+    console.log('Hola estoy aqui desde ngOnInit');
+ 
+    this.empleadoService.getAllEmployee().subscribe(
+      {
+        next: (datos) => {
+          console.log(datos);
+          this.listEmpleado = datos.data;
+          this.mensajes.add({ severity: 'success', summary: 'Satisfactorio', detail: 'Exito' });
+        },error: (err) => {
+          console.log(err);
+          this.mensajes.add({ severity: 'error', summary: 'Error', detail: 'Hubo un problema' });
+        }
+      }
+    )
+   }
+
+  iniColumnaTabla(){
+    this.columnTabla = [ 
+      {
+        field: 'id', header: 'ID'
+      },
+      {
+        field: 'name', header: 'Nombre Empleado'
+      },
+      {
+        field: 'salary', header: 'Salario Empleado'
+      },
+      {
+        field: 'edad', header: 'Edad'
+      }
+     ]
+  }
+
+  RegresarInicio(){
+    //llamada de la ruta para el inicio
+    this.rutas.navigate(['inicio']);
+  }
 }
