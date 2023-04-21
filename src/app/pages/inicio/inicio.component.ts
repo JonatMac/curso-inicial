624afd8e3c660f96be9dc32948288750dfb54c38
JonatMac <jonathan15-08@hotmail.com>
2023-04-21 18:18:41 -0500

    Cambios 20230421

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-inicio',
@@ -12,13 +13,15 @@ export class InicioComponent implements OnInit{
 nombre = 'Jonathan';
 apellido = 'Macías';
 loadingVisible = false;
-formUsuario!: FormGroup;
+formEmpleado!: FormGroup;
 
 constructor(private ruta: Router,
-  private mensajes: MessageService ){
+  private mensajes: MessageService,
+  private serviceEmpleado: EmpleadoService ){
 }
   ngOnInit(): void {
     this.buildFormulario();
+    
   }
 
 visualizarLoading(){
@@ -33,20 +36,34 @@ IrPaginaTabla(){
 }
 
 buildFormulario(){
-  this.formUsuario = new FormGroup(
+  this.formEmpleado = new FormGroup(
     {
-      nombre: new FormControl('', [Validators.required]),
-      apellido: new FormControl('', [Validators.required])
+      name: new FormControl('', [Validators.required]),
+      salary: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required])
     }
   );
 }
 
 verificar(){
-  console.log(this.formUsuario);
-  if (this.formUsuario.valid){
+  console.log(this.formEmpleado);
+  if (this.formEmpleado.valid){
     this.mensajes.add({ severity: 'success', summary: 'Formulario Correcto', detail: 'Exito' });
   } else {
     this.mensajes.add({ severity: 'error', summary: 'Error', detail: 'Por favor llene los campos requeridos' });
   }
+}
+
+crearEmpleado(){
+  this.serviceEmpleado.createEmployee(this.formEmpleado.value.name,this.formEmpleado.value.salary,this.formEmpleado.value.age).subscribe({
+    next: (resp)=>{
+      console.log(resp);
+      this.mensajes.add({ severity: 'success', summary: 'Empleado creado con exito', detail: 'Exito' });
+    },
+    error: (resp)=>{
+      console.log(resp);
+      this.mensajes.add({ severity: 'error', summary: 'Error', detail: 'Hubo un error.' });
+    }
+  });
 }
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { IEmpleado } from '../interfaces/empleadosInterface';
 
 const API_GET_ALL_EMPLOYEE = environment.API_GET_ALL_EMPLOYEE;
+const API_POST_CREATE_EMPLOYEE = environment.API_POST_CREATE_EMPLOYEE;
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,26 @@ export class EmpleadoService {
 
   constructor(private http: HttpClient) { }
 
+  createHeader(){
+    let headers: HttpHeaders;
+      headers=new HttpHeaders().set('Content-Type','application/json; charset=utf-8')
+      .set('Acces-Control-Allow_Origin','*')
+      //.set('Authorization','Bearer' + token)
+    return headers;
+  }
+
   getAllEmployee(){
     return this.http.get<IEmpleado>(API_GET_ALL_EMPLOYEE);
+  }
+
+  createEmployee(nombre: string, sal: string, edad: string){
+    //const headers = this.createHeader();
+    let body = {
+      name: nombre,
+      salary: sal,
+      age: edad
+    };
+    return this.http.post(API_POST_CREATE_EMPLOYEE,body);
+    //return this.http.post(API_POST_CREATE_EMPLOYEE,body,{headers});
   }
 }
